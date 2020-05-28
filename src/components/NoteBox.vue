244d93187a73479166cf843bb455be4bb89109bf
anemoneflower <sallylucky@naver.com>
2020-05-28 15:12:08 +0900

    ADD: make component structure

---
+++ src/components/NoteBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="NoteBox">
+    <h1>{{ book }}</h1>
+    <h1>{{ date }}</h1>
+    <h1>{{ essayTitle }}</h1>
+    <h3>{{ author }}</h3>
+    <div class="contents">
+      <p>{{ contents }}</p>
+    </div>
+    <h3>Installed CLI Plugins</h3>
+    <h3>Essential Links</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoteBox",
+  props: {
+    book: String,
+    date: String,
+    essayTitle: String,
+    author: String,
+    contents: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/ReadNote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="read-my-note">
+    <h1>This is a note page</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <NoteBox
+      title="Welcome to Your Vue.js App"
+      author="a"
+      book="book"
+      date="july"
+      content="bla bla~~"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NoteBox from "@/components/NoteBox.vue";
+
+export default {
+  name: "ReadMyNote",
+  components: {
+    NoteBox
+  }
+};
+</script>
